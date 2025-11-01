ad08f42deb36a9efc9f99878625e70ddbf9878f7
Atal Prateek Barla <prateek.atal@gmail.com>
2025-11-01 08:26:38 +0530

    s

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -91,7 +91,7 @@
             </ul>
          </div>
       </li>
-      <li class="nav-item d-none">
+      <li class="nav-item">
          <a class="nav-link" data-bs-toggle="collapse" href="#incomeMenu" role="button" aria-expanded="false" aria-controls="incomeMenu">
          <i class="fa-solid fa-money-bill-transfer"></i> Incomes
          <i class="fas fa-chevron-down ms-auto"></i>

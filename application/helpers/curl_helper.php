bee0e53d5a3d1d6855505b6514233b660aa6bdab
atal-tripledots <135303345+atal-tripledots@users.noreply.github.com>
2025-11-20 16:11:57 +0530

    s

--- application/helpers/curl_helper.php
+++ application/helpers/curl_helper.php
@@ -39,3 +39,240 @@
             return $price;
 		}  
 	}
+    
+	if(!function_exists('checkApiStatus')) {
+  		function checkApiStatus() {
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.nowpayments.io/v1/status',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            $array=json_decode($response,true);
+            if(!empty($array['message']) && strtolower($array['message'])=="ok"){
+                return TRUE;
+            }
+            else{
+                return FALSE;
+            }
+		}  
+	}
+
+	if(!function_exists('getAuthToken')) {
+  		function getAuthToken($data) {
+            $payload=json_encode($data);
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.nowpayments.io/v1/auth',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>$payload,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            $array=json_decode($response,true);
+            if(!empty($array['token'])){
+                return $array['token'];
+            }
+            else{
+                return '';
+            }
+		}  
+	}
+
+	if(!function_exists('getAvailableCoins')) {
+  		function getAvailableCoins() {
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.nowpayments.io/v1/merchant/coins',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'x-api-key: '.NP_API_KEY
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            $array=json_decode($response,true);
+            if(!empty($array['selectedCurrencies'])){
+                return $array['selectedCurrencies'];
+            }
+            else{
+                return array();
+            }
+		}  
+	}
+
+	if(!function_exists('getMinimumAmount')) {
+  		function getMinimumAmount($from,$to,$fixed="False",$userfee="False") {
+
+            $params="currency_from=$from&currency_to=$to&fiat_equivalent=usd&is_fixed_rate=$fixed&is_fee_paid_by_user=$userfee";
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.nowpayments.io/v1/min-amount?'.$params,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'x-api-key: '.NP_API_KEY
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            $array=json_decode($response,true);
+            if(!empty($array)){
+                return $array;
+            }
+            else{
+                return array();
+            }
+		}  
+	}
+
+	if(!function_exists('getBalance')) {
+  		function getBalance() {
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.nowpayments.io/v1/balance',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'x-api-key: '.NP_API_KEY
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            $array=json_decode($response,true);
+            if(!empty($array)){
+                return $array;
+            }
+            else{
+                return array();
+            }
+		}  
+	}
+
+	if(!function_exists('validateAddress')) {
+  		function validateAddress($data) {
+            $payload=json_encode($data);
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.nowpayments.io/v1/payout/validate-address',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>$payload,
+            CURLOPT_HTTPHEADER => array(
+                'x-api-key: '.NP_API_KEY,
+                'Content-Type: application/json'
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            $array=json_decode($response,true);
+            
+            if(empty($array) && strtolower($response)=='ok'){
+                return TRUE;
+            }
+            else{
+                return FALSE;
+            }
+		}  
+	}
+
+	if(!function_exists('createPayout')) {
+  		function createPayout($authdata,$data) {
+            $payload=json_encode($data);
+            $curl = curl_init();
+
+            $token=getAuthToken($authdata);
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.nowpayments.io/v1/payout',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS =>$payload,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$token,
+                'x-api-key: '.NP_API_KEY,
+                'Content-Type: application/json'
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            $array=json_decode($response,true);
+
+            if(!empty($array)){
+                return $array;
+            }
+            else{
+                return array();
+            }
+		}  
+	}

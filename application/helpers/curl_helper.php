f90729f7665e1c527e8ca55e96d26404d8335df5
atal-tripledots <135303345+atal-tripledots@users.noreply.github.com>
2025-11-20 17:02:56 +0530

    s

--- application/helpers/curl_helper.php
+++ application/helpers/curl_helper.php
@@ -169,6 +169,39 @@
 		}  
 	}
 
+	if(!function_exists('getMinimumWithdrawal')) {
+  		function getMinimumWithdrawal($coin) {
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.nowpayments.io/v1/payout-withdrawal/min-amount/'.$coin,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'x-api-key: '.NP_API_KEY
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            $array=json_decode($response,true);
+            if(!empty($array)){
+                return $array;
+            }
+            else{
+                return array();
+            }
+		}  
+	}
+
 	if(!function_exists('getBalance')) {
   		function getBalance() {
 
